a1986394572ac6ac3c15d8069e4bd2f9bbd8f351
Dilshard <mrdilshard@gmail.com>
2022-11-19 16:17:12 +0530

    Update 100%

--- csd77/update.php
+++ csd77/update.php
@@ -24,9 +24,30 @@ if($status){
 ?>
 
 <form method="post">
-    <input type="text" name="" value="<?php echo $fullname; ?>"> <br>
-    <input type="text" name="" value="<?php echo $gender; ?>"> <br>
-    <input type="text" name="" value="<?php echo $add; ?>"> <br>
-    <input type="text" name="" value="<?php echo $email; ?>"> <br>
-    <input type="submit" value="Save">
+    <input type="text" name="txtfname" value="<?php echo $fullname; ?>"> <br>
+    <input type="text" name="txtgender" value="<?php echo $gender; ?>"> <br>
+    <input type="text" name="txtaddress" value="<?php echo $add; ?>"> <br>
+    <input type="text" name="txtemail" value="<?php echo $email; ?>"> <br>
+    <input type="submit" name="btnsave" value="Save">
 </form>
+
+<?php
+
+    if(isset($_POST['btnsave'])){
+        $fulName = $_POST['txtfname'];
+        $gender = $_POST['txtgender'];
+        $add = $_POST['txtaddress'];
+        $email = $_POST['txtemail'];
+
+        $sql2 = "UPDATE `personal` SET `fullName` = '$fulName', gender = '$gender', address = '$add', email = '$email' WHERE `id` = $id";
+
+        $status = mysqli_query($con,$sql2);
+
+        if($status){
+            header("Location: view.php");
+        }else{
+            echo "Error - updated!";
+        }
+    }
+
+?>
